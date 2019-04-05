211f4f79ee089eaed693bdace99a72f0e774cb2d
k0il <k0il.inside@gmail.com>
2019-04-05 23:17:52 +0700

    ok

---
+++ Assets/moveObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveObject : MonoBehaviour
+{
+    public float speed = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Project");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Click Left");
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Click RIght");
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Click Up");
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Click Down");
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
